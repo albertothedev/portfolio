7cb9e05ac25bea281560f48e6a7704f16d17a51e
Alberto Vilches <albertothedev@protonmail.com>
2022-05-02 13:27:51 +0200

    refactor(ui): remove unnecessary comments

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -8,8 +8,6 @@ import App from "./App";
 
 import "./styles/main.scss";
 
-// import reportWebVitals from "./reportWebVitals";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelopeSquare, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -17,17 +15,7 @@ library.add(faLinkedin, faGithub, faEnvelopeSquare, faTimesCircle);
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <div className="app">
-      <About />
-      <Projects />
-      <Contact />
-    </div> */}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
